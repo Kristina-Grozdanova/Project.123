3a384891bfcaac495a773fd0cdfd5b47a41b7e89
Kristina <kristinagrozdanova914@yahoo.com>
2021-03-01 17:36:05 +0100

    m

---
+++ src/package2/Class1.java
@@ -0,0 +1,11 @@
+package package2;
+
+public class Class1 {
+
+
+public static void main(String[]args){
+        System.out.println("God morning");
+        System.out.println(":)");
+        System.out.println("Wake up??!");
+
+        }}
